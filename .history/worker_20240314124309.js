d53221dde9c8253e05a8b1ffe14d9aa398f39a3a
adonijah01 <adonijahsingoei@gmail.com>
2024-03-14 12:47:55 +0300

    files manager

--- .history/worker_20240314124309.js
+++ .history/worker_20240314124309.js
@@ -62,11 +62,11 @@ userQueue.process(async (job, done) => {
   }
   console.log(`Welcome ${user.email}!`);
   try {
-    const mailSubject = 'Welcome to ALX-Files_Manager by Adonijah Kiplimo';
+    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
     const mailContent = [
       '<div>',
       '<h3>Hello {{user.name}},</h3>',
-      'Welcome to <a href="https://github.com/Adonijah01/alx-files_manager">',
+      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
       'ALX-Files_Manager</a>, ',
       'a simple file management API built with Node.js by ',
       '<a href="https://github.com/Adonijah01">Adonijah Kiplimo</a>. ',
